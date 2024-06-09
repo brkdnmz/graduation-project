b3246283f0d2731c06c70c098c8d3495289dd3be
brkdnmz <brkdnmz99@gmail.com>
2024-06-10 02:11:36 +0300

    Remove old SQLite migration files

--- prisma/migrations/20240115011224_added_contest_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Contest" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "startsAt" DATETIME NOT NULL,
-    "endsAt" DATETIME NOT NULL
-);
